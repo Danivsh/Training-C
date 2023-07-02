7fd274f589adb7c6d8d4f45bf6a87612f615c24d
Danil Ivshin <rroninway@gmail.com>
2023-07-02 14:10:15 +0400

    make lesson2

--- lesson2/hello.c
+++ lesson2/hello.c
@@ -2,8 +2,14 @@
 
 int main(void) 
 {
-    int var;
-    var = 100;
-    
+    double d1, d2, d3, d4;
+
+    d1 = 10.0f;
+    d2 = -7.;
+    d3 = 1e2;
+    d4 = 5e-3;
+
+    int sizee_float = sizeof(float);
+
     return 0;
 }
